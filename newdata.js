9f479b2014edc20288a4d1645c676cbfce9d3ac4
Deepak Maurya <74600250+helodeepakji@users.noreply.github.com>
2022-06-29 06:48:09 +0530

    Add files via upload

---
+++ newdata.js
@@ -0,0 +1,24 @@
+let replay = {
+
+    "what is your name": "my name is chatbot 2.O",
+
+    "hi": "hello my name is chatbot 2.O how may i help you",
+
+    "hii": "hello my name is chatbot 2.O how may i help you",
+
+    "hai": "hello my name is chatbot 2.O how may i help you",
+
+    "hello": "hello my name is chatbot 2.O how may i help you",
+
+    "hii": "hello kese h aap",
+
+    "i love you": "I Love You 2",
+
+    "who are you": "my name is chatbot 2.O",
+
+    "how are you": "I am Fine",
+
+    "how old are you": " I am infinity in age",
+
+    "i am fine": "great"
+}
